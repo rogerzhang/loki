5ac5201c5092e63ca0a0081072e8b91e04f57900
Chris Van Rensburg <chris.vanrensburg@ringcentral.com>
2014-11-13 15:39:36 -0800

    Normalizing to single tab per indent level, as per other modules in this project.

--- source/js/Loki/Utils/ExcludeUtils.js
+++ source/js/Loki/Utils/ExcludeUtils.js
@@ -1,63 +1,63 @@
 Uize.module({
-    name: 'Loki.Utils.ExcludeUtils',
-    required:'Uize.Services.FileSystem',
-    builder: function(_superclass) {
-        'use strict';
+	name: 'Loki.Utils.ExcludeUtils',
+	required:'Uize.Services.FileSystem',
+	builder: function(_superclass) {
+		'use strict';
 
-        return Uize.package({
-            loadExclides: function(fileName) {
-                console.log('Reading excludes from: "' + fileName + '"');
+		return Uize.package({
+			loadExclides: function(fileName) {
+				console.log('Reading excludes from: "' + fileName + '"');
 
-                var resultFunction = Uize.returnTrue;
+				var resultFunction = Uize.returnTrue;
 
-                var fs = Uize.Services.FileSystem.singleton();
-                if (fs.fileExists({path: fileName})) {
-                    var excludesContent = fs.readFile({
-                        path: fileName
-                    });
-                    var excludes = excludesContent ? excludesContent.split('\n') : [];
-                    excludes = filterExcludes(excludes);
-                    var checkerRegexp = prepareRegexp(excludes);
+				var fs = Uize.Services.FileSystem.singleton();
+				if (fs.fileExists({path: fileName})) {
+					var excludesContent = fs.readFile({
+						path: fileName
+					});
+					var excludes = excludesContent ? excludesContent.split('\n') : [];
+					excludes = filterExcludes(excludes);
+					var checkerRegexp = prepareRegexp(excludes);
 
-                    if (checkerRegexp) {
-                        resultFunction = function(_stringInfo) {
-                            return !checkerRegexp.test(_stringInfo.key);
-                        }
-                    }
-                }
+					if (checkerRegexp) {
+						resultFunction = function(_stringInfo) {
+							return !checkerRegexp.test(_stringInfo.key);
+						}
+					}
+				}
 
-                return  {
-                    isTranslatableString: resultFunction
-                };
+				return  {
+					isTranslatableString: resultFunction
+				};
 
-                function filterExcludes(excludes) {
-                    var result = [];
-                    excludes.forEach(function(item){
-                        var value = item ? item.replace(/((([#])|([//]{2,})).*|\s)/g, '') : '';
-                        if (value) {
-                            result.push(value);
-                        }
-                    });
-                    return result;
-                }
+				function filterExcludes(excludes) {
+					var result = [];
+					excludes.forEach(function(item){
+						var value = item ? item.replace(/((([#])|([//]{2,})).*|\s)/g, '') : '';
+						if (value) {
+							result.push(value);
+						}
+					});
+					return result;
+				}
 
-                function prepareRegexp(excludes) {
-                    var result = [];
+				function prepareRegexp(excludes) {
+					var result = [];
 
-                    excludes.forEach(function(item){
-                        var value = item ? item.replace(/([\.{}\)\(])/g, '\\$1').replace(/\*/g, '.*').replace(/[?]/g, '.') : '';
-                        if (value) {
-                            result.push('^' + value + '$');
-                        }
-                    });
-                    if (result.length > 0){
-                        return new RegExp('(' + result.join('|')+ ')');
-                    }
+					excludes.forEach(function(item){
+						var value = item ? item.replace(/([\.{}\)\(])/g, '\\$1').replace(/\*/g, '.*').replace(/[?]/g, '.') : '';
+						if (value) {
+							result.push('^' + value + '$');
+						}
+					});
+					if (result.length > 0){
+						return new RegExp('(' + result.join('|')+ ')');
+					}
 
-                    return null;
-                }
-            }
-        });
-    }
+					return null;
+				}
+			}
+		});
+	}
 });
 
