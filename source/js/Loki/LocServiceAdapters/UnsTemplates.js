be7126641ea2706879309bb36f81e06f004173dc
Chris Van Rensburg <chris.vanrensburg@ringcentral.com>
2015-10-12 12:05:49 -0700

    Proper pseudo-localization of HTML for the UNS templates.

--- source/js/Loki/LocServiceAdapters/UnsTemplates.js
+++ source/js/Loki/LocServiceAdapters/UnsTemplates.js
@@ -10,7 +10,10 @@ Uize.module ({
 	superclass:'Uize.Services.LocAdapter',
 	required:[
 		'Uize.Util.RegExpComposition',
-		'Uize.Util.RegExpComposition.WordSplitterHtml'
+		'Uize.Util.RegExpComposition.WordSplitterHtml',
+		'Uize.Util.Matchers.AttributeMatcher',
+		'Uize.Loc.Pseudo.Xml',
+		'Uize.Str.Has'
 	],
 	builder:function (_superclass) {
 		'use strict';
@@ -27,7 +30,14 @@ Uize.module ({
 			_wordSplitterRegExpComposition = Uize.Util.RegExpComposition.WordSplitterHtml.extend ({
 				token:/\{\{(.+?)\}\}/,
 				wordSplitter:/{htmlTag}|{htmlEntity}|{token}|{whitespace}|{punctuation}|{number}/
-			})
+			}),
+			_xmlPseudoLocalizeOptions = {
+				attributeMatcher:Uize.Util.Matchers.AttributeMatcher.resolve ([
+					'title',
+					'img@alt',
+					'[input|textarea]@placeholder'
+				])
+			}
 		;
 
 		return _superclass.subclass ({
@@ -36,6 +46,33 @@ Uize.module ({
 					return _primaryLanguageResourcePath.replace (
 						_resourceFileRegExp,
 						'$1$2__$3__$4__' + _language.replace ('-','_') + '$6'
+					);
+				},
+
+				stringHasHtml:function (_stringPath) {
+					return Uize.Str.Has.hasSuffix (_stringPath [0].match (_resourceFileRegExp) [3],'_html');
+				},
+
+				pseudoLocalizeString:function (_stringInfo,_pseudoLocalizeOptions) {
+					var
+						m = this,
+						_stringValue = _stringInfo.value,
+						_pseudoLocalized =
+							m.stringHasHtml (_stringInfo.path,_stringValue) &&
+							Uize.Loc.Pseudo.Xml.pseudoLocalize (
+								_stringValue
+									.replace (/(<(?:img|input|meta)\s+[^>]+[^\/])>/gi,'$1/>')
+									.replace (/<br>/gi,'<br/>'),
+								Uize.copyInto (_xmlPseudoLocalizeOptions,_pseudoLocalizeOptions)
+							)
+					;
+					return (
+						!_pseudoLocalized ||
+							// the string hasn't been pseudo-localized yet (because it didn't contain HTML)
+						_pseudoLocalized.length < _stringValue.length * .8
+							// there must be some other errors in the HTML that prevent it from being parsed correctly
+							? _superclass.doMy (m,'pseudoLocalizeString',arguments)
+							: _pseudoLocalized
 					);
 				},
 
